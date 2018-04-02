22a773ef866c9bb5bd5d97cc299330a8722b3fb9
Andrzej <ekspensum@interia.pl>
2018-04-02 23:02:19 +0200

    parking - statystyka wg dnia

--- parking/src/Statystyka.java
+++ parking/src/Statystyka.java
@@ -1,11 +1,14 @@
+import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class Statystyka extends JDialog {
     private JPanel contentPane;
@@ -16,17 +19,41 @@ public class Statystyka extends JDialog {
     private JTable tabelaStat;
     private JPanel panelKalendarz;
     private JLabel wprowadzDateLabel;
+    private JButton buttonStatOk;
     private JDateChooser wybierzDate;
     private Calendar cld;
+    private SimpleDateFormat sdf;
+    private String dataOut, dataKalendarz;
+    private LocalDateTime dataTime;
 
     public Statystyka() {
+        sdf = new SimpleDateFormat("yyyy-MM-dd");
         setContentPane(contentPane);
         setModal(true);
 //        getRootPane().setDefaultButton(buttonOK);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                int x=0;
+                for (int i=0; i<Wrapper.getRejestrParkowan().size(); i++){
+                    tabelaStat.setValueAt(null, i,0);
+                    tabelaStat.setValueAt(null, i,1);
+                    tabelaStat.setValueAt(null, i,2);
+                    tabelaStat.setValueAt(null, i,3);
+                    tabelaStat.setValueAt(null, i,4);
+                    if (Wrapper.getRejestrParkowan().get(i).getDataOut() != null && wybierzDate.getDate() != null){
+                        if (Wrapper.getRejestrParkowan().get(i).getDataOut().getMonth().getValue() == (wybierzDate.getJCalendar().getMonthChooser().getMonth()+1)){
+                            if (Wrapper.getRejestrParkowan().get(i).getDataOut().getDayOfMonth() == wybierzDate.getJCalendar().getDayChooser().getDay()){
+                                tabelaStat.setValueAt(x+1, x, 0);
+                                tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getNrRejString(), x, 1);
+                                tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getRodzajPojazdu(), x, 2);
+                                tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataIn(), x, 3);
+                                tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataOut(), x, 4);
+                                x++;
+                            }
+                        }
+                    }
+                }
             }
         });
 
@@ -50,14 +77,27 @@ public class Statystyka extends JDialog {
 //                onCancel();
 //            }
 //        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+//        buttonStatOk.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+////                System.out.println(sdf.format(wybierzDate.getJCalendar().getDate().getDay()));
+////                System.out.println(sdf.format(Wrapper.getRejestrParkowan().get(24).getDataIn().getDayOfYear()));
+//                System.out.println(Wrapper.getRejestrParkowan().get(24).getDataIn().getMonth().getValue());
+//                System.out.println(wybierzDate.getDate().getMonth());
+//                System.out.println(DateFormat.getDateTimeInstance().format(wybierzDate.getDateEditor().getDate()));
+////                System.out.println(wybierzDate.getJCalendar().getDate().equals(Wrapper.getRejestrParkowan().get(24).getDataIn()));
+//            }
+//        });
+
         setSize(900,400);
         setVisible(true);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
-    }
+//    private void onOK() {
+//        // add your code here
+//        dispose();
+//    }
 
     private void onCancel() {
         // add your code here if necessary
@@ -69,6 +109,7 @@ public class Statystyka extends JDialog {
         cld = Calendar.getInstance();
         wybierzDate = new JDateChooser(cld.getTime());
         wybierzDate.setPreferredSize(new Dimension(150,20));
+        wybierzDate.setDateFormatString("yyyy-MM-dd");
         wprowadzDateLabel.setLabelFor(wybierzDate);
         panelKalendarz = new JPanel();
         panelKalendarz.setSize(200,30);
@@ -91,14 +132,14 @@ public class Statystyka extends JDialog {
         tabelaStat.getColumnModel().getColumn(5).setPreferredWidth(50);
         tabelaStat.getColumnModel().getColumn(6).setPreferredWidth(50);
         modelTabeliStat.setRowCount(Wrapper.getRejestrParkowan().size());
-        for (int i=0; i<Wrapper.getRejestrParkowan().size(); i++){
-            tabelaStat.setValueAt(i+1, i, 0);
-            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getNrRejString(), i, 1);
-            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getRodzajPojazdu(), i, 2);
-            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataIn(), i, 3);
-            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataOut(), i, 4);
-
-        }
+//        for (int i=0; i<Wrapper.getRejestrParkowan().size(); i++){
+//            tabelaStat.setValueAt(i+1, i, 0);
+//            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getNrRejString(), i, 1);
+//            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getRodzajPojazdu(), i, 2);
+//            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataIn(), i, 3);
+//            tabelaStat.setValueAt(Wrapper.getRejestrParkowan().get(i).getDataOut(), i, 4);
+//
+//        }
 
     }
 }
