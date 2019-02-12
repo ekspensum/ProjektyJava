aa14fa9cb90f6d194646630c611e1d7ff8e44851
ekspensum <ekspensum@interia.pl>
2019-02-12 19:50:41 +0100

    ShopApp - testy, poprawki

--- ShopApp/ShopAppBeans/src/test/java/integration/BeansIT.java
+++ ShopApp/ShopAppBeans/src/test/java/integration/BeansIT.java
@@ -2,9 +2,21 @@ package integration;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -12,16 +24,13 @@ import org.jboss.arquillian.junit.InSequence;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import pl.shopapp.beans.BasketBean;
 import pl.shopapp.beans.BasketData;
 import pl.shopapp.beans.ProductBean;
+import pl.shopapp.beans.ProductBeanLocal;
 import pl.shopapp.beans.SessionData;
 import pl.shopapp.beans.TransactionBean;
 import pl.shopapp.beans.UserBean;
@@ -40,7 +49,7 @@ import pl.shopapp.entites.User;
 import pl.shopapp.entites.UserRole;
 
 @RunWith(Arquillian.class)
-public class UserBeanIT {
+public class BeansIT {
 	
 	@Deployment
 	public static JavaArchive createDeployment() {
@@ -54,6 +63,10 @@ public class UserBeanIT {
 
 	@EJB
 	private UserBeanLocal ubl;
+	@EJB
+	private ProductBeanLocal pbl;
+	
+	private byte [] buffer = {1,1,1};
 	
 	private String login = null;
 	private String password = null;
@@ -73,31 +86,27 @@ public class UserBeanIT {
 	private String regon = "123456789";
 	private String phoneNo = "+012 345 67 89";
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+    @PersistenceContext
+    EntityManager em;
+    
+    @Inject
+    UserTransaction ut;
+	
 	@Test
 	@InSequence(1)
-	public final void testCustomer() {
+	public final void prepareTests() {
 		ubl.addRole("admin");
 		ubl.addRole("customer");
 		ubl.addRole("operator");
-		ubl.setSettingsApp(6, 20, 1, 2, 5, 12, 30, 1);
+		assertEquals(3, ubl.getRoleList().size());
+		assertTrue(ubl.setSettingsApp(6, 20, 1, 2, 5, 12, 30, 1));		
+	}
+	
+	@Test
+	@InSequence(2)
+	public final void testCustomer() throws InterruptedException {
+
 		Validation valid = new Validation(ubl.getSettingsApp());
-		
 		login = "customer1";
 		password = "Customer11";
 		assertTrue(valid.loginValidation(login));
@@ -115,17 +124,20 @@ public class UserBeanIT {
 		assertTrue(valid.nameValidation(companyName));
 		assertTrue(valid.nipValidation(taxNo));
 		assertTrue(valid.regonValidation(regon));		
-		assertTrue(ubl.addCustomer(login, password, firstName, lastName, pesel, zipCode, country, city, street, streetNo, unitNo, email, isCompany, companyName, taxNo, regon));
+		assertTrue(ubl.addCustomer(login, password, firstName, lastName, pesel, zipCode, country, city, street, streetNo, unitNo, email, isCompany, companyName, taxNo, regon));	
 		assertTrue(ubl.updateCustomer(login, password, firstName, lastName, pesel, zipCode, country, city, street, streetNo, unitNo, email, isCompany, companyName, taxNo, regon, 1));
 		User u = ubl.findUser(1);
 		Customer c = ubl.findCustomer(u);
 		assertEquals("firstName", c.getFirstName());
 		assertTrue(ubl.setActiveCustomer(c.getId(), false));
 		assertTrue(ubl.setActiveCustomer(c.getId(), true));
+		assertTrue(ubl.findUserLogin(login));
+		SessionData sd = ubl.loginUser(login, valid.passwordToCode(password));	
+		assertEquals(2, sd.getIdRole());
 	}
 	
 	@Test
-	@InSequence(2)
+	@InSequence(3)
 	public final void testAdmin() {
 		login = "admin1";
 		password = "Admin11";
@@ -137,14 +149,18 @@ public class UserBeanIT {
 		assertTrue(valid.nameValidation(lastName));
 		assertTrue(valid.telephoneNoValidation(phoneNo));
 		assertTrue(valid.emailValidation(email));
-		assertTrue(ubl.addAdmin(firstName, lastName, phoneNo, email, login, password, 1));
-		List<Admin> al = ubl.getAdminsData();
-		assertEquals(2, al.size());
-		assertTrue(ubl.updateAdminData(1, al.get(1).getId(), login, true, firstName, lastName, phoneNo, email));
+		assertTrue(ubl.addAdmin(firstName, lastName, phoneNo, email, login, valid.passwordToCode(password), 1));
+//		note: is 2 admins because first admin was added with addAdmin method (default) 
+		assertEquals(2, ubl.getAdminsData().size());
+		assertTrue(ubl.updateAdminData(1, ubl.getAdminsData().get(1).getId(), login, true, firstName, lastName, phoneNo, email));
+		SessionData sd = ubl.loginAdmin(login, valid.passwordToCode(password));
+		assertEquals(1, sd.getIdRole());
+		assertTrue(ubl.findUserLogin(login));
+		assertEquals(ubl.getUsersAdminData().size()+1, ubl.getAdminsData().size());
 	}
 	
 	@Test
-	@InSequence(3)
+	@InSequence(4)
 	public final void testOperator() {
 		login = "operator1";
 		password = "Operator11";
@@ -156,10 +172,47 @@ public class UserBeanIT {
 		assertTrue(valid.nameValidation(lastName));
 		assertTrue(valid.telephoneNoValidation(phoneNo));
 		assertTrue(valid.emailValidation(email));
-		assertTrue(ubl.addOperator(firstName, lastName, phoneNo, email, login, password, 1));
+		assertTrue(ubl.addOperator(firstName, lastName, phoneNo, email, login, valid.passwordToCode(password), 1));
 		List<Operator> ol = ubl.getOperatorsData();
 		assertEquals(1, ol.size());
 		assertTrue(ubl.updateOperatorData(1, ol.get(0).getId(), login, true, firstName, lastName, phoneNo, email));
+		SessionData sd = ubl.loginUser(login, valid.passwordToCode(password));
+		assertEquals(3, sd.getIdRole());
+		assertTrue(ubl.findUserLogin(login));
+		assertEquals(ubl.getUsersOperatorData().size(), ubl.getOperatorsData().size());
 	}
-
+	@Test
+	@InSequence(5)
+	public final void testCategory() {
+		assertTrue(pbl.addCategory("categoryName1", buffer, 3));
+		assertTrue(pbl.addCategory("categoryName2", buffer, 3));
+		assertTrue(pbl.addCategory("categoryName3", buffer, 3));
+		assertEquals(3, pbl.listCategory().size());
+	}
+	
+	@Test
+	@InSequence(6)
+	public final void testProduct() {
+		byte [] buffer = {};
+		int [] categoryToEdit = new int [9];
+		categoryToEdit[0] = 1;
+		categoryToEdit[1] = 3;
+		List<Integer> helperListCat = new ArrayList<>();
+		helperListCat.add(1);
+		helperListCat.add(3);
+		assertTrue(pbl.addProduct("productName", "productDescription", 11.22, 13, buffer, helperListCat, 3));
+		assertEquals(1, pbl.getProduct(1).getId());
+		
+		String updateProductWithoutImage = "UPDATE Product SET name = 'productName', description = 'productDescription', price = 33.44, unitsInStock = 3, dateTime = '"+LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))+"', id = 1 WHERE id = 1";
+		try {
+			ut.begin();
+			assertEquals(1, em.createQuery(updateProductWithoutImage).executeUpdate());
+			ut.commit();
+		} catch (SecurityException | IllegalStateException | NotSupportedException | SystemException | RollbackException
+				| HeuristicMixedException | HeuristicRollbackException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		assertEquals(3, pbl.getProduct(1).getUnitsInStock());
+	}	
 }
