03995e85abeb5e0905eefe9533c12514d10d0998
ekspensum <ekspensum@interia.pl>
2019-06-13 22:51:30 +0200

    ShopAppOpenShift

--- ShopAppOpenShift/ShopAppBeans/src/main/java/pl/shopapp/beans/ProductBeanLocal.java
+++
@@ -1,9 +0,0 @@
-package pl.shopapp.beans;
-
-import javax.ejb.Local;
-
-@Local
-public interface ProductBeanLocal extends ProductBeanRemote {
-	
-	
-}
--- ShopAppOpenShift/ShopAppWebService/src/main/java/pl/shopapp/webservice/JAXActivator.java
+++
@@ -1,9 +0,0 @@
-package pl.shopapp.webservice;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JAXActivator extends Application {
-	//should be empty 
-}
