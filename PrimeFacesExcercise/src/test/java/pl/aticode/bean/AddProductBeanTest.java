dfd8ab8d5846ea11b309b0c8e2182f10cfe18f11
ekspensum <ekspensum@interia.pl>
2021-06-07 17:17:20 +0200

    Tests

---
+++ PrimeFacesExcercise/src/test/java/pl/aticode/bean/AddProductBeanTest.java
@@ -0,0 +1,84 @@
+package pl.aticode.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddProductBeanTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testAddProductBean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProductRepository() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSelectedCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCategoryList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCategoryRepository() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProductRepository() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSelectedCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCategoryList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCategoryRepository() {
+		fail("Not yet implemented");
+	}
+
+}
